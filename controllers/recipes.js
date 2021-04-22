b4af8aceb9d53f8974e402f7a7d0567923140cb2
pritckat <pritckat@gmail.com>
2021-04-22 11:29:42 -0400

    move form to recipes page

--- controllers/recipes.js
+++ controllers/recipes.js
@@ -1,5 +1,14 @@
+const recipe = require("../models/recipe")
+
 module.exports = {
-    getRecipes: (req,res)=>{
-        res.render('./pages/index.ejs')
+    getRecipes: async (req,res)=>{
+        try{
+            const recipeItems = await recipe.find()
+            console.log('hi')
+            console.log(recipeItems)
+            res.render('./pages/recipes.ejs', {recipes: recipeItems})
+        }catch(err){
+        console.log(err)
+        }
     }
 }
