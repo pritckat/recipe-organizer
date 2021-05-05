18f76aebf9115a511f45306eccf5ed7a563ee5d4
pritckat <pritckat@gmail.com>
2021-05-05 17:30:58 -0400

    recipe update form populates when edit is pressed

--- public/js/main.js
+++ public/js/main.js
@@ -2,5 +2,8 @@ const editButton = document.querySelector('#editRecipe')
 editButton.addEventListener('click', viewEditForm)
 
 function viewEditForm() {
-    
+    const regular = document.querySelector('.regular')
+    const form = document.querySelector('.updateForm')
+    regular.toggleAttribute('hidden')
+    form.toggleAttribute('hidden')
 }
