69e417a80ec610b634979170efd81ac9ec05474f
pritckat <pritckat@gmail.com>
2021-05-11 10:21:13 -0400

    profile now shows all user's recipes

--- controllers/home.js
+++ controllers/home.js
@@ -1,11 +1,19 @@
+const Recipe = require("../models/recipe")
+  
   module.exports = {
     getIndex: (req,res)=>{
         res.render('index.ejs')
     },
 
-    getProfile: (req,res)=>{
-      console.log(req.user)
-      const user = req.user.userName
-      res.render('profile.ejs', {user: user})
+    getProfile: async (req,res)=>{
+      try {
+        console.log(req.user)
+        const user = req.user.userName
+        const recipes = await Recipe.find({user: req.user._id})
+        console.log(recipes)
+        res.render('profile.ejs', {user: user, recipes: recipes})
+      } catch(err){
+        console.log(err)
+      }
     }
 }
